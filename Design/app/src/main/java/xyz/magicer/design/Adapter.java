046989f2bf2b708ada3b0c7f7860037b7cf15ce0
zhangtongshuang <magicer_zhang@163.com>
2017-07-25 22:04:05 +0800

    design

---
+++ Design/app/src/main/java/xyz/magicer/design/Adapter.java
@@ -0,0 +1,69 @@
+package xyz.magicer.design;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by lenovo on 2017/7/21.
+ */
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    private List<String> mDatas;
+
+    public Adapter(List<String> mDatas) {
+        this.mDatas = mDatas;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Glide.with(holder.itemView.getContext())
+                .load(R.drawable.image)
+                .into(holder.ivImg);
+        holder.itemView.setOnClickListener(v -> {
+            Context context = holder.itemView.getContext();
+            Intent intent = new Intent(context,DetailActivity.class);
+            context.startActivity(intent);
+
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.iv_img)
+        ImageView ivImg;
+        @BindView(R.id.tv_title)
+        TextView tvTitle;
+        @BindView(R.id.tv_content)
+        TextView tvContent;
+
+        ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
---
+++ Design/app/src/main/java/xyz/magicer/design/MainActivity.java
@@ -0,0 +1,90 @@
+package xyz.magicer.design;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by lenovo on 2017/7/19.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.recycler)
+    RecyclerView recycler;
+    @BindView(R.id.navigation)
+    NavigationView navigation;
+    @BindView(R.id.drawer)
+    DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        _init();
+        listener();
+        loadData();
+    }
+
+    private void listener() {
+        navigation.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_music:
+                    break;
+                case R.id.nav_movie:
+                    break;
+                case R.id.nav_setting:
+                    break;
+            }
+            drawer.closeDrawer(GravityCompat.START);
+            Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+            startActivity(intent);
+            return false;
+        });
+
+    }
+
+
+    private void _init() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
+        toggle.syncState();
+        drawer.addDrawerListener(toggle);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void loadData() {
+        ArrayList<String> datas = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            datas.add("");
+        }
+        Adapter adapter = new Adapter(datas);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recycler.setLayoutManager(layoutManager);
+        recycler.setAdapter(adapter);
+    }
+}
